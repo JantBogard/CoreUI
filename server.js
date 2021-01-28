9c8979af881747fae978a8f1a86cc6c5b3eb9735
JantBogard <joancenkonga@gmail.com>
2021-01-28 18:56:11 +0100

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/myfirstappineroku'));
+app.use(express.static(__dirname + '/dist/@coreui/coreui-free-angular-admin-template'));
 
 app.get('/*', function(req,res) {
 
-res.sendFile(path.join(__dirname+'/dist/myfirstappineroku/index.html'));
+res.sendFile(path.join(__dirname+'/dist/@coreui/coreui-free-angular-admin-template/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
